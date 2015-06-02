1219c392b8384657f95ec356972d3534bcf95a1b
Mo Li <MoLi@Mos-iMac.local>
2015-06-02 18:05:54 -0400

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Mo Li on 6/2/15.
+//  Copyright (c) 2015 Mo Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
